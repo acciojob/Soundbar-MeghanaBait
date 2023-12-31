2ba357042d835f738cbe2b4b527fb45320cad6f2
MeghanaBait <133511757+MeghanaBait@users.noreply.github.com>
2023-12-31 10:00:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+let currentSound = null;
+
+function playSound(soundFile) {
+    if (currentSound) {
+        currentSound.pause();
+        currentSound.currentTime = 0;
+    }
+
+    currentSound = new Audio(`sounds/${soundFile}`);
+    currentSound.play();
+}
+
+function stopSound() {
+    if (currentSound) {
+        currentSound.pause();
+        currentSound.currentTime = 0;
+    }
+}
